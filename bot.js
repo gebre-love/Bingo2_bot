83dd02f1f4541227a1ceb53f5d10b880e254c2d4
gebre-love <gebreawoke4th@gmail.com>
2026-01-22 13:04:28 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,30 @@
 const { Telegraf, Markup } = require('telegraf');
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// ጨዋታው ሲያልቅ ውጤት መቀበያ
+let usersDB = {};
+
 app.post('/secure-win', (req, res) => {
     const { userId, winAmount, secretKey } = req.body;
-    if (secretKey === "BINGO_SECRET_99") {
-        bot.telegram.sendMessage(userId, `🎉 እንኳን ደስ አለዎት! ${winAmount} ብር አሸንፈዋል።`);
+    if (secretKey === "BINGO_SECRET_99" && usersDB[userId]) {
+        usersDB[userId].balance += winAmount;
+        bot.telegram.sendMessage(userId, `🎉 እንኳን ደስ አለዎት! ${winAmount} ብር አሸንፈው ሂሳብዎ ${usersDB[userId].balance} ETB ሆኗል።`);
     }
     res.sendStatus(200);
 });
 
 bot.start((ctx) => {
-    ctx.reply(`ሰላም ${ctx.from.first_name}! እንኳን ወደ አዲስ ቢንጎ መጡ።`, 
+    const userId = ctx.from.id;
+    if (!usersDB[userId]) usersDB[userId] = { balance: 100 };
+    ctx.reply(`ሰላም ${ctx.from.first_name}! ሂሳብዎ፡ ${usersDB[userId].balance} ETB`, 
     Markup.inlineKeyboard([
-        [Markup.button.webApp('🎮 ጨዋታ ጀምር', `https://gebre-love.github.io/Bingo_game1/?userId=${ctx.from.id}`)]
+        [Markup.button.webApp('🎮 ጨዋታ ጀምር', `https://gebre-love.github.io/Bingo_game2/?userId=${userId}`)]
     ]));
 });
 
